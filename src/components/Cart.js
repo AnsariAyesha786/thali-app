7c01dfde7d8111b336576f865be35ddd2f897c05
ansariayesha <ansariayesha2017@gmail.com>
2022-02-25 13:06:06 +0530

    thali redux-app

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,37 +1,35 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useSelector ,useDispatch } from 'react-redux'; 
 import { RemoveItem } from '../redux/slice';
 const Cart = () => {
   const thali=useSelector(state=>state.counter.thali)
+  console.log(thali)
   const despatch=useDispatch()
-//   const[arr,setArr]=useState([])
   const Remove=(id)=>{
     despatch(RemoveItem(id))
 
   }
-//   {
-//       thali.map((e)=>{
-//           let a=[]
-//           a.push(e.totalPrice)
-//          console.log(a)
-          
-//       })
-//     }
+var sum=0
+for(const item of thali){
+  sum=sum+item.totalPrice
+}
   return (
     <div className='container mx-3'>
 <h1 className='text-center text-danger'>Your Food</h1>
 <div className='row'>
 {
     thali.map((ele,ind)=>(
-        
-            <div className="card col-3" key={ind} >
+        <div className='col-3'>
+            <div className="card " key={ind} >
   <img src={ele.image} className="card-img-top" alt="..." width="100px" height="200px"/>
   <div className="card-body">
     <h5 className="card-title">{ele.name}</h5>
     <p className="card-text">Quantity: {ele.quantity}</p>
-    <p>{ele.price}</p>
-    <p>{ele.totalPrice}</p>
-        <a href="#" onClick={()=>Remove(ind)} className="btn btn-primary">Remove</a>
+    <p>Price:{ele.price}</p>
+    <p>totalPrice:{ele.totalPrice}</p>
+    <button onClick={()=>Remove(ind)} className="btn btn-primary">Remove</button>
+        
+  </div>
   </div>
 </div>
     
@@ -40,7 +38,7 @@ const Cart = () => {
     )
     )
 }
-
+<h3 className='text-center text-success'>Your Sub total price :{sum}</h3>
 </div>
     </div>
   )
